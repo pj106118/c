8b5e65d611afd5ad7a40dc4c1fa35cc5ac7bacae
潘晶 <1583030729@qq.com>
2018-12-11 13:26:22 +0800

    打印9乘9乘法表

---
+++ able_1204/able_1204/test.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+void main()
+{
+	int i, j, num;
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			num = i*j;
+			printf("%d*%d=%d\t",i*j=num);
+		}
+		printf("\n");
+	}
+	system("pause");
+}
