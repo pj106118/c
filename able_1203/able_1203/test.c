a6fa5ef71bfc97515a3b012f07f6e7e182b320b1
潘晶 <1583030729@qq.com>
2018-12-16 21:28:27 +0800

    当输入的一个字符为小写字母a到z,则输出大写字母A到Z，若为大写字母a到z,则输出小写字母a到z，若为数字，则直接跳出

--- able_1203/able_1203/test.c
+++ able_1203/able_1203/test.c
@@ -1,26 +1,59 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-void main()
+#define N 10
+int init(int arr1[N])
 {
-	int i, j, n;
-	printf("请输入一个数字");
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
+	arr1[N] = { 1,2,3,4,5 }
+	for (int i = 0; i <= 5; ++i)
 	{
-		for (j = 2 * i - 1; j > 0; j--)
-		{
-			printf("*");
-		}
-		printf("\n");
+		arr1[i] = 0;
 	}
-	for (i = n - 1; i >= 0; i--)
-	{
-		for (j = 2 * i - 1; j > 0; j--)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
-	system("pause");
+	return 0;
+}        //这个函数是为了对数组进行初始化
+
+void empty(int arr1[N])
+{
+	int i;
+	for (i = 0; i < N; ++i)
+		arr1[i] = 0;
+	printf("清空完成！");
+}              //这个函数是为了对数组清零
+
+void reverse(int arr1[N])
+{
+	int arr2[N], i, j;
+	for (i = 0, j = N - 1; i < N, j >= 0; ++i, j--)
+		arr2[i] = arr1[j];
+	for (i = 0; i < N; ++i)
+		arr1[i] = arr2[i];
+	for (i = 0; i < N; ++i)
+		printf("arr[%d]=%2d ", i + 1, arr1[i]);
+}            //这个函数是为了对数组进行逆置
+
+int main()
+{
+	int arr[N], a;
+printf("**           1：初始化数组         **\n");
+printf("**           2：清空数组           **\n");
+printf("**           3：逆置数组           **\n");
+printf("**           4：退出               **\n");
+printf("*************************************\n\n");
+printf("请选择：");
+scanf("%d", &a);
+switch (a)
+{
+case 1:
+	init(arr);
+	break;
+case 2:
+	empty(arr);
+	break;
+case 3:
+	reverse(arr);
+	break;
+dautle:
+	break;
+}
+system("pause");
+return 0;
 }
