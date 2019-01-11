4993814059673c152be128bc8bedcf190d492267
潘晶 <1583030729@qq.com>
2019-01-11 13:22:37 +0800

    求平均值的方式 float  num = a+（b-a）/ 2.0

---
+++ able_2019_0109/test.c
@@ -0,0 +1,22 @@
+﻿//第二种求平均值的方式 float  num = a+（b-a）/ 2.0
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+void everage(int* a, int* b)
+{
+	float num = 0;
+	num = *a + ( *b - *a ) / 2.0;
+	printf("%f\n", num);
+}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	printf("请输入两个数：\n");
+	scanf("%d,%d", &a, &b);
+	everage(&a, &b);
+	system("pause");
+	return 0;
+}
