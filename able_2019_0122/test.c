42599531e45e5c837362bfb47467ffaa7060b7f9
潘晶 <1583030729@qq.com>
2019-01-22 21:23:41 +0800

    简单的一级指针传参

---
+++ able_2019_0122/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print(int *p, int sz) 
+{ 
+	int i = 0; 
+	for (i = 0; i<sz; i++) 
+	{ 
+		printf("%d\n", *(p + i));
+	}
+} 
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9 }; 
+	int *p = arr;  
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	system("pause");
+	return 0;
+}
