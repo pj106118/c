9f5d73a4bf643f0c30957101bece4363145ca530
潘晶 <1583030729@qq.com>
2019-01-23 22:25:44 +0800

    这是模拟一个只会加减乘除的计算器

---
+++ able_2019_0123/test.c
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <stdlib.h>
+
+int add(int a, int b) 
+{ 
+	return a + b;
+} 
+int sub(int a, int b)
+{
+	return a - b; 
+}
+int mul(int a, int b) 
+{
+	return a*b;
+}
+int div(int a, int b) 
+{
+	return a / b; 
+}
+int main()
+{
+	int x, y;
+	int input = 1;
+	int ret = 0;
+	while (input)
+	{
+		printf("*************************\n");
+		printf("  1:add           2:sub  \n");
+		printf("  3:mul           4:div  \n");
+		printf("*************************\n");
+		printf("请选择：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("输入操作数：");
+			scanf("%d %d", &x, &y);
+			ret = add(x, y);
+			break;
+		case 2:
+			printf("输入操作数：");
+			scanf("%d %d", &x, &y);
+			ret = sub(x, y);
+			break;
+		case 3:
+			printf("输入操作数：");
+			scanf("%d %d", &x, &y);
+			ret = mul(x, y);
+			break;
+		case 4:
+			printf("输入操作数：");
+			scanf("%d %d", &x, &y);
+			ret = div(x, y);
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+		printf("ret = %d\n", ret);
+	}
+	system("pause");
+	return 0;
+}
